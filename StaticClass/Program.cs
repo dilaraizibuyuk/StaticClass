f083fbcab9bd665f3f602ec8fb7896d8ff684af6
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-11 16:58:54 +0300

    Proje dosyası ekle.

---
+++ StaticClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // User u1 = new User(); //static olduğu için nesne oluşturmaya izin vermiyor
+
+            User.topla(15, 8);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ StaticClass/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClass
+{
+    public static class User//class static
+    {
+        //public int number;//değişken static olmadığı için tanımlamadı!
+        //public void yazdir() //static olmayan metot kabul edilmez
+
+        public static void topla(int number1, int number2)
+        {
+            Console.WriteLine("Toplam:"+(number1+number2));
+        }
+    }
+}
